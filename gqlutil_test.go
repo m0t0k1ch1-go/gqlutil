697bc60cb02fe8a6e78b85699a8436aa7b2a6819
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-08-29 04:20:24 +0900

    test: add test cases for encoding/decoding cursor

--- gqlutil_test.go
+++ gqlutil_test.go
@@ -12,6 +12,27 @@ type CursorPayload struct {
 }
 
 func TestEncodeToCursor(t *testing.T) {
+	t.Run("failure", func(t *testing.T) {
+		tcs := []struct {
+			name string
+			in   any
+			want string
+		}{
+			{
+				"func",
+				func() {},
+				"",
+			},
+		}
+
+		for _, tc := range tcs {
+			t.Run(tc.name, func(t *testing.T) {
+				_, err := gqlutil.EncodeToCursor(tc.in)
+				require.ErrorContains(t, err, tc.want)
+			})
+		}
+	})
+
 	t.Run("success", func(t *testing.T) {
 		tcs := []struct {
 			name string
@@ -41,6 +62,32 @@ func TestEncodeToCursor(t *testing.T) {
 }
 
 func TestDecodeCursor(t *testing.T) {
+	t.Run("failure", func(t *testing.T) {
+		tcs := []struct {
+			name string
+			in   string
+			want string
+		}{
+			{
+				"invalid base64",
+				"a",
+				"",
+			},
+			{
+				"invalid json",
+				"eyJvZmZzZXQiOiIifQ",
+				"",
+			},
+		}
+
+		for _, tc := range tcs {
+			t.Run(tc.name, func(t *testing.T) {
+				_, err := gqlutil.DecodeCursor[CursorPayload](tc.in)
+				require.ErrorContains(t, err, tc.want)
+			})
+		}
+	})
+
 	t.Run("success", func(t *testing.T) {
 		tcs := []struct {
 			name string
