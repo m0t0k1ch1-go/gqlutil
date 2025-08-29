0ca32a17a8cb820bc9a700f497407250cbf608a8
m0t0k1ch1 <m0t0k1ch1.310@gmail.com>
2025-08-29 16:51:04 +0900

    feat: add funcs for must encoding/decoding cursor

--- gqlutil.go
+++ gqlutil.go
@@ -15,6 +15,16 @@ func EncodeToCursor[T any](v T) (string, error) {
 	return base64.RawURLEncoding.EncodeToString(b), nil
 }
 
+// MustEncodeToCursor panics if the input is invalid.
+func MustEncodeToCursor[T any](v T) string {
+	cursor, err := EncodeToCursor(v)
+	if err != nil {
+		panic(err)
+	}
+
+	return cursor
+}
+
 // DecodeCursor decodes a cursor string produced by EncodeToCursor.
 func DecodeCursor[T any](cursor string) (v T, err error) {
 	var b []byte
@@ -27,4 +37,14 @@ func DecodeCursor[T any](cursor string) (v T, err error) {
 	err = json.Unmarshal(b, &v)
 
 	return
+}
+
+// MustDecodeCursor panics if the input is invalid.
+func MustDecodeCursor[T any](cursor string) T {
+	v, err := DecodeCursor[T](cursor)
+	if err != nil {
+		panic(err)
+	}
+
+	return v
 }
